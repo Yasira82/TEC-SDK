629aaba90f6dfe341ef3d66804eae7838ce38986
Tec ( titan-elite-commerce ) <yasserrr.fox17@gmail.com>
2026-04-11 12:00:30 +0200

    Add initial tsup configuration file

---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry:       ['src/index.ts'],
+  format:      ['cjs', 'esm'],
+  dts:         true,
+  sourcemap:   true,
+  clean:       true,
+  outDir:      'dist',
+  splitting:   false,
+  treeshake:   true,
+  target:      'es2020',
+  outExtension({ format }) {
+    return { js: format === 'esm' ? '.mjs' : '.js' };
+  },
+});
